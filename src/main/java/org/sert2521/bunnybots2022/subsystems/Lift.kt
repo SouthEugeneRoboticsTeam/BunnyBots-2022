e945d8fa16ae0bb60e676cc2448435e0b56bc5eb
fruzzmuffin <benji.urbancic@gmail.com>
2022-12-10 12:40:46 -0800

    Fixed spacing and getting height

--- src/main/java/org/sert2521/bunnybots2022/subsystems/Lift.kt
+++ src/main/java/org/sert2521/bunnybots2022/subsystems/Lift.kt
@@ -9,35 +9,39 @@ import org.sert2521.bunnybots2022.constants
 object Lift : SubsystemBase() {
     val motor = CANSparkMax(-1, CANSparkMaxLowLevel.MotorType.kBrushless)
     val liftDownLimitSwitch = DigitalInput(-1)
+
     override fun periodic(){
         if(liftDownLimitSwitch.get()) {
             motor.encoder.position = 0.0
         }
 
-        if(motor.encoder.position >= constants.liftEncoderMax){
+        if(getHeight() >= constants.liftEncoderMax){
             if(motor.get()>0){
                 motor.stopMotor()
             }
-        }else if(motor.encoder.position<=0.0){
+        }else if(getHeight() <=0.0){
             if(motor.get()<0){
                 motor.stopMotor()
             }
         }
     }
+
     fun getHeight(): Double{
         return motor.encoder.position * constants.liftEncoderRatio
     }
     fun setMotor(speed:Double){
-        if((motor.encoder.position >= constants.liftEncoderMax) && (speed > 0.0)){
+        if((getHeight() >= constants.liftEncoderMax) && (speed > 0.0)){
             return
-        }else if((motor.encoder.position <= 0.0) && (speed < 0.0)){
+        }else if((getHeight() <= 0.0) && (speed < 0.0)){
             return
         }
         motor.set(speed)
     }
+
     fun atBottom():Boolean{
         return liftDownLimitSwitch.get()
     }
+
     fun stop(){
         motor.stopMotor()
     }
